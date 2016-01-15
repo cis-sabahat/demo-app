677f78068fbbc60c03464fa421dcbd640efeee71
cis-saba <sabahat.a@cisinlabs.com>
2016-01-15 17:41:40 +0530

    Changes in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'demo_app'
+set :repo_url, 'https://github.com/cis-sabahat/demo-app.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
